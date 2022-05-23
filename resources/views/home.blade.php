ae30d5ca776d8670b7d8097561fabc6250fb37f7
simoneluca00 <si.zaino@libero.it>
2022-05-23 19:25:02 +0200

    Main added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,29 @@
 @extends('layouts.layout')
 
 @section('headTitle', 'Home')
+
+@section('content')
+<div id="container-comics">
+    <div class="labelComic">
+        current series
+    </div>
     
+    <div class="row-products">
+        @foreach ( $comics as $product )
+        <div class="card">
+            <img src=" {{ $product['thumb'] }} " alt="copertina di {{$product['title']}}" class="card-img">
+            <h5>
+                {{ $product['title'] }}
+            </h5>
+        </div>
+        @endforeach
+    </div>
+
+    <div class="containerBtn">
+        <button class="loadMore">
+          load more
+        </button>
+    </div>
+
+</div>
+@endsection
